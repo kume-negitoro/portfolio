54202188227bc111e969f42109dc8c23ddc31fc5
kume-negitoro <kume.negitoro@gmail.com>
2022-05-30 01:19:52 +0900

    update webpack.dev.ts

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -1,9 +1,18 @@
+import path from 'path'
 import merge from 'webpack-merge'
 import common from './webpack.common'
+import 'webpack-dev-server'
 
 const config = merge(common, {
-    mode: 'production',
+    mode: 'development',
     devtool: 'inline-source-map',
+    performance: {
+        hints: false,
+    },
+    devServer: {
+        open: true,
+        watchFiles: path.join(__dirname, 'www'),
+    },
 })
 
 export default config
