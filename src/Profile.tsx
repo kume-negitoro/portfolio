e9f548dc553903e4bc59e4842dda4305d7e1774e
kume-negitoro <kume.negitoro@gmail.com>
2022-06-01 03:02:49 +0900

    add Profile

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -3,10 +3,17 @@ import profile from './images/profile.png'
 
 const Profile = () => {
     return (
-        <>
-            <img src={profile} />
-            <p>ねぎまぐろ</p>
-        </>
+        <div className="flex  bg-pink-200">
+            <img className="flex-none w-128 rounded-full p-5" src={profile} />
+            <div className="flex flex-col justify-center pl-5">
+                <div className="flex-1 grid place-items-end">
+                    <p className="underline text-center text-5xl">ねぎまぐろ</p>
+                </div>
+                <div className="flex-1 grid place-items-center">
+                    <p className="text-center">はじめまして！</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
