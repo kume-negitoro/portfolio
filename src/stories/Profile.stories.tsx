5975325f41a570ec6441054ca2f9892d1a83f99f
kume-negitoro <kume.negitoro@gmail.com>
2022-06-03 20:19:36 +0900

    add Profile.stories.tsx

---
+++ src/stories/Profile.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Profile from '../Profile'
+
+export default {
+    component: Profile,
+    title: 'Profile',
+}
+
+const Template = () => <Profile />
+
+export const Default = Template.bind({})
