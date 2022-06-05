0a6936bb78ffa7b0b2fd8e1fdff02b71fe8d3af1
kume-negitoro <kume.negitoro@gmail.com>
2022-06-05 14:24:43 +0900

    move profile to center

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -3,16 +3,25 @@ import profile from './images/profile.png'
 
 const Profile = () => {
     return (
-        <div className="flex  bg-pink-200">
-            <img className="flex-none w-128 rounded-full p-5" src={profile} />
-            <div className="flex flex-col justify-center pl-5">
-                <div className="flex-1 grid place-items-end">
-                    <p className="underline text-center text-5xl">ねぎまぐろ</p>
-                </div>
-                <div className="flex-1 grid place-items-center">
-                    <p className="text-center">はじめまして！</p>
+        <div className="flex  bg-pink-100">
+            <div className="flex-1"></div>
+            <div className="flex flex-auto">
+                <img
+                    className="flex-none w-72 rounded-full p-5"
+                    src={profile}
+                />
+                <div className="flex flex-col justify-center pl-5">
+                    <div className="flex-1 grid place-items-end">
+                        <p className="underline text-center text-5xl">
+                            ねぎまぐろ
+                        </p>
+                    </div>
+                    <div className="flex-1 grid place-items-center">
+                        <p className="text-center">はじめまして！</p>
+                    </div>
                 </div>
             </div>
+            <div className="flex-1"></div>
         </div>
     )
 }
