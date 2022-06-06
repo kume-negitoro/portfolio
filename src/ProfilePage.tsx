82df3118e14a4fc97c7c5f22b0073a3c64a5daec
kume-negitoro <kume.negitoro@gmail.com>
2022-06-07 00:19:56 +0900

    add infomation markdown files

--- src/ProfilePage.tsx
+++ src/ProfilePage.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
 import Profile from './Profile'
 import SkillCard from './SkillCard'
 import { list as skillList } from './skills'
+import intro from './informations/introduction.md'
+import poprop from './informations/poprop.md'
 
 const ProfilePage = () => {
     return (
@@ -13,13 +16,7 @@ const ProfilePage = () => {
                     <section className="pt-5 pb-5">
                         <h2 className="text-2xl">私について</h2>
                         <div className="break-word whitespace-pre-wrap">
-                            公立はこだて未来大学 大学院 システム情報科学研究科
-                            システム情報科学専攻 知能情報科学領域 1年生です。
-                            中学生の頃にIT技術に興味を持ち始め、国立函館工業高等専門学校に進学。
-                            プログラミングやネットワークなどIT技術の基礎を学んだほか、ゲーム製作やハッカソンなども経験してきました。
-                            現在はTypeScriptでのフロントエンド開発の実践や、流行の機械学習技術の基礎を勉強しています。
-                            大学院では、ライフログ映像からの興味領域の推定について研究しています。
-                            とにかく技術が好きです。新しい技術を使ってみたり、実装を考えたり、成果物が動いた時が一番楽しいので、エンジニアとして開発の仕事ができると良いなあと思っています。
+                            <ReactMarkdown>{intro}</ReactMarkdown>
                         </div>
                     </section>
                     <section className="pt-5 pb-5">
@@ -39,14 +36,7 @@ const ProfilePage = () => {
 
                             <div>
                                 <p>作品紹介</p>
-                                <div>
-                                    Hack U SENDAI
-                                    に向けて制作したWebアプリケーションです。
-                                    Hack U
-                                    をハックする、をテーマにWord2Vecを利用したブレーンストーミング支援アプリ。一人でもブレーンストーミングができることや発展性が高いことが評価され、Hack
-                                    U SENDAI 2019 で最優秀賞を受賞しました。
-                                    アイデアが思いつかないのを逆手に、アイデア出し支援のためのアプリケーションを開発することになった。
-                                </div>
+                                <ReactMarkdown>{poprop}</ReactMarkdown>
                             </div>
                             <div>
                                 <p>開発期間</p>
